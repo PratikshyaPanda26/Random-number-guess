11b17ef911bc6f2276f5ff09699071114bb758a3
Pratikshya Panda <pratikshya2605@gmail.com>
2026-02-04 10:49:03 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,13 @@
+import random
+n = random.randint(1, 100)
+a=-1
+guesses = 0
+while(a!= n):
+    guesses += 1
+    a = int(input("Guess a number: "))
+    if(a> n):
+        print("Lower no please")
+    else:
+        print("Higher no please")
+
+print(f"You have guessed the no. correctly in {guesses} attempt")
